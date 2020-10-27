cbe76a756897928a9158d79289dfd193d837c3bb
Kriti Chandel <kritichandel.05@gmail.com>
2020-10-27 10:15:42 +0530

    springData-jpa-crud

---
+++ springData-jpa/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// tno need to write any code!
+	
+}
